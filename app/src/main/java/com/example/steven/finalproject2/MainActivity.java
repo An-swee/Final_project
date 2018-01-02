7790e7bd59978170c3b653b7218fbf169b7198ae
matt19970817 <chen850907@gmail.com>
2018-01-03 00:32:12 +0800

    系統整合，整合date + search

--- app/src/main/java/com/example/steven/finalproject2/MainActivity.java
+++ app/src/main/java/com/example/steven/finalproject2/MainActivity.java
@@ -36,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<HashMap<String, Object>> Item = new ArrayList<HashMap<String, Object>>();
     ArrayList<HashMap<String, Object>> serach = new ArrayList<HashMap<String, Object>>();
     String qdqw = "wqrwqqweq";
+    String q12q3dqw = "wqrwqqweq";
+    String q123d3qw = "wqrwqqweq";
+    String q123dq324w = "wqrwqqweq";
+    String q12343dqw = "wqrwqqweq";
+    String q123d2w34qw = "wqrwqqweq";
+    String q123432dqw = "wqrwqqweq";
     String q123dqw = "wqrwqqweq";
 
     @Override
